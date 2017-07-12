b0813088328b8c2e47c2465a2d619401c899e84b
huwenboshi <shihuwenbo@ucla.edu>
2017-07-12 00:19:18 -0700

    updated code to simulate trait

--- 3_simple_trait_sim/sim_trait.R
+++ 3_simple_trait_sim/sim_trait.R
@@ -1,8 +1,3 @@
-# simulation parameter
-nindv = 10000
-nsnp = 100
-hsq = 0.6
-
 # simulate a nindv * nsnp genotype matrix
 sim_geno = function(nindv, nsnp) {
 
@@ -21,7 +16,9 @@ sim_geno = function(nindv, nsnp) {
     return(list("af"=af, "geno"=geno))
 }
 
-# first simulate genotype matrix
+# first simulate a genotype matrix at 100 SNPs for 10,000 individuals
+nindv = 10000
+nsnp = 500
 sim = sim_geno(nindv, nsnp)
 sim_af = sim$af
 X = sim$geno
@@ -51,5 +48,28 @@ sim_trait = function(X, hsq) {
     return(Y)
 }
 
-# simulate the trait
-Y = sim_trait(X, hsq)
+# simulate the trait at different heritability
+pdf("zscore_dist.pdf")
+all_hsq = c(0.1, 0.2, 0.3, 0.4, 0.5)
+for(hsq in all_hsq) {
+
+    # simulate trait a a specific heritability
+    Y = sim_trait(X, hsq)
+
+    # perform a simple GWAS
+    # first standardize the data
+    Y = scale(Y)
+    X = scale(X)
+
+    # iterate through each SNP
+    nsnp = ncol(X)
+    zscores = rep(0, nsnp)
+    for(i in 1:nsnp) {
+        fit = lm(Y ~ X[,i])
+        beta_hat = coef(summary(fit))[2,1]
+        beta_hat_se = coef(summary(fit))[2,2]
+        zscores[i] = beta_hat / beta_hat_se
+    }
+    hist(zscores, xlim=c(-15, 15))
+}
+dev.off()
